fec1de314977065a447043df2ac34ab8bda8d777
chuanshaof <chuanshaof@gmail.com>
2023-10-01 22:49:13 +0800

    chatbot design to be done

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -1,42 +1,63 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import Head from 'next/head';
+import Layout from '@components/Layout';
+
 import { useContext } from 'react';
 import { ShipContext } from 'src/context/shipContext';
 
 function Chatbot() {
-  const [prompt, setPrompt] = useState('');
-  const [botReply, setBotReply] = useState('');
+    const [prompt, setPrompt] = useState('');
+    const [botReply, setBotReply] = useState('');
 
-  const { message } =   useContext(ShipContext);
+    const { message } = useContext(ShipContext);
 
-  const handleChat = async () => {
-    try {
-      const response = await axios.post('http://127.0.0.1:5000/chat', {
-        json: message,
-        prompt: prompt
-      });
+    // var data = message.slice(0, 10)
 
-      setBotReply(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    // console.log(data)
 
-  return (
-    <div>
-      <h1>Chat Bot</h1>
-      <div>
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button onClick={handleChat}>Send</button>
-      </div>
-      <p>{botReply}</p>
-    </div>
-  );
+    const handleChat = async () => {
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/chat', {
+                json: JSON.stringify(message),
+                prompt: prompt
+            });
+
+            setBotReply(response);
+
+            console.log(botReply)
+        } catch (error) {
+            setBotReply("Chat API is not working, please configure your replicate API key")
+        }
+    };
+
+    return (
+
+        <Layout>
+            <Head>
+                <title>059CRAWL</title>
+                <meta name="description" content="PSA Codesprint 2023" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div>
+                <h1>Welcome to PSA's Chat Bot! Ask any questions regarding the data!</h1>
+                <div>
+                    <input
+                        type="text"
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                    />
+                    <button onClick={handleChat}>Send</button>
+                </div>
+
+                {/* <p>{botReply}</p> */}
+            </div>
+
+
+        </Layout>
+    );
 }
 
 export default Chatbot;
