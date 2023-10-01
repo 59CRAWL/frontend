a20f8d672400eba2bfb7cd9617523f898abd9ab6
chuanshaof <chuanshaof@gmail.com>
2023-10-01 18:52:34 +0800

    replaced ship icon

--- src/components/Routing/DynamicRouting.js
+++ src/components/Routing/DynamicRouting.js
@@ -1,40 +1,36 @@
 import L from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
-import icon from '../../assets/cargo-ship.png';
+import icon from '../../../public/leaflet/images/cargo-ship.png';
 
 const CreateRoutineMachineLayer = (props) => {
-    console.log(props)
     var coords = [L.latLng(props.end[0], props.end[1])]
 
+    // FIXME: shipIcon not loading
     let shipIcon = new L.Icon({
         iconUrl: icon,
         iconSize: [25, 50],
         iconAnchor: [-16, 20],
     });
 
-    var TooltipClass = {
-        'className': 'class-tooltip'
-      }
-
-    const instance = L.Routing.control({ 
+    const instance = L.Routing.control({
         waypoints: coords,
         createMarker: function (i, wp, nWps) {
             if (i === 0) {
-                return L.marker(wp.latLng,{icon: shipIcon}).bindTooltip(props.ship.vesselName,{permanent: true,direction: "left",...TooltipClass}) //`Current vessel location LatLng : (${wp.latLng.lat},${wp.latLng.lng})`
+                return L.marker(wp.latLng).bindTooltip(props.ship.vesselName, { permanent: true, direction: "left" })
             }
         },
 
         lineOptions: {
             // missingRouteStyles: [{ color: "#6FA1EC", weight: 6 }],
             styles: [{ color: "#6FA1EC", weight: 0 }],
-          },
-          show: false,
-          addWaypoints: false,
-          routeWhileDragging: true,
-          draggableWaypoints: false,
-          fitSelectedRoutes: true,
-          showAlternatives: false,
+        },
+        show: false,
+        addWaypoints: false,
+        routeWhileDragging: true,
+        draggableWaypoints: false,
+        fitSelectedRoutes: true,
+        showAlternatives: false,
     });
     return instance;
 };
