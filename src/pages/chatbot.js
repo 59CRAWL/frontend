ffa90cb81ecd91ec59a2a903478b12ddd3b91a74
xuehuapiaopiaoo <68467928+xuehuapiaopiaoo@users.noreply.github.com>
2023-10-01 23:01:55 +0800

    chatbot layout

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import { useContext } from 'react';
 import { ShipContext } from 'src/context/shipContext';
+import Layout from '@components/Layout';
+import Head from 'next/head';
 
 function Chatbot() {
   const [prompt, setPrompt] = useState('');
@@ -22,21 +24,27 @@ function Chatbot() {
       console.error(error);
     }
   };
-
   return (
-    <div>
-      <h1>Chat Bot</h1>
+    <Layout>
+      <Head>
+        <title>059CRAWL</title>
+        <meta name="description" content="PSA Codesprint 2023" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='center rcenter'>
+      <h1 className='cheader'>Chat Bot</h1>
       <div>
         <input
+        className='rounded chatinput'
           type="text"
           value={message}
           onChange={(e) => setPrompt(e.target.value)}
         />
-        <button onClick={handleChat}>Send</button>
+        <button type='submit' className='ttext' onClick={handleChat}>Send</button>
       </div>
       <p>{botReply}</p>
-    </div>
-  );
-}
-
+      </div>
+    </Layout>
+  )
+  }
 export default Chatbot;
