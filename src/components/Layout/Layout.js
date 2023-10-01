18c51822dd2c83b4e551b4d3f1454dd8b57eb813
chuanshaof <chuanshaof@gmail.com>
2023-10-01 22:29:29 +0800

    removed chatbot for now

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-import Chatbot from '@components/Chatbot';
+// import Chatbot from '@components/Chatbot';
 
 import styles from './Layout.module.scss';
 
@@ -14,7 +14,7 @@ const Layout = ({ children, className, ...rest }) => {
       </Head>
       <Header />
       <main className={styles.main}>{children}</main>
-      <Chatbot />
+      {/* <Chatbot /> */}
       <Footer />
     </div>
   );
