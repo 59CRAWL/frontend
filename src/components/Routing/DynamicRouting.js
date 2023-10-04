9790257c82242aa37536f15a09e1e081c0206c75
99wanglin <wanglinsim@gmail.com>
2023-10-05 03:47:23 +0800

    working on play

--- src/components/Routing/DynamicRouting.js
+++ src/components/Routing/DynamicRouting.js
@@ -1,23 +1,22 @@
 import L from "leaflet";
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
-import icon from '../../../public/leaflet/images/cargo-ship.png';
 
 const CreateRoutineMachineLayer = (props) => {
     var coords = [L.latLng(props.end[0], props.end[1])]
 
     // FIXME: shipIcon not loading
     let shipIcon = new L.Icon({
-        iconUrl: icon,
+        iconUrl: 'leaflet/images/cargo-ship.png',
         iconSize: [25, 50],
-        iconAnchor: [-16, 20],
+        iconAnchor: [8, 10],
     });
 
     const instance = L.Routing.control({
         waypoints: coords,
         createMarker: function (i, wp, nWps) {
             if (i === 0) {
-                return L.marker(wp.latLng).bindTooltip(props.ship.vesselName, { permanent: true, direction: "left" })
+                return L.marker(wp.latLng, {icon: shipIcon}).bindTooltip(props.ship.vesselName, { permanent: true, direction: "left" })
             }
         },
 
--- src/pages/simulate.js
+++ src/pages/simulate.js
@@ -7,6 +7,8 @@ import styles from '@styles/Home.module.scss';
 import { useContext, useEffect, useState } from 'react';
 import { ShipContext } from 'src/context/shipContext';
 import RoutingMachine from '@components/Routing/RoutingMachine';
+import { FaPause, FaPlay } from 'react-icons/fa';
+
 
 const DEFAULT_CENTER = [1.262822, 103.786229]
 
@@ -41,11 +43,13 @@ var counter = 1;
 var removedShips = []
 
 export default function Simulate() {
-  const { message } = useContext(ShipContext)
+  const { message } = useContext(ShipContext);
   
-  const [shipsShowing, setShipsShowing] = useState(message? [message[0]]: [])
+  const [shipsArray, setShipsArray] = useState(message? [message[0]]: []);
 
-  
+  // var shipsPlaying = false;
+
+  const [shipsPlaying, setShipsPlaying] = useState(false);
 
   function nextShip() {
     if (!message) {
@@ -56,37 +60,49 @@ export default function Simulate() {
       return;
     }
     // console.log(shipsShowing);
-    var newShipsShowing = [...shipsShowing]
-    const index = newShipsShowing.findIndex((ship) => message[counter].berth == ship.berth)
-        // console.log(index)
+    var newShipsArray = [...shipsArray]
+    console.log(counter)
+    console.log(message)
+    console.log(shipsArray)
+    const index = newShipsArray.findIndex((ship) => message[counter].berth == ship.berth)
+    console.log(index)
     // console.log(message[counter].vesselName)
     if (index == -1) {
-      newShipsShowing.push(message[counter]);
+      newShipsArray.push(message[counter]);
     } else {
-      removedShips.push({index: index, ship: newShipsShowing[index]})
-      newShipsShowing[index] = message[counter]
+      console.log('remove')
+      removedShips.push({index: index, ship: newShipsArray[index]})
+      newShipsArray[index] = message[counter]
     }
     counter += 1;
-    setShipsShowing(newShipsShowing);
+    setShipsArray(newShipsArray);
     // console.log(message)
-    // console.log(shipsShowing)
+    console.log(newShipsArray)
     // console.log(removedShips)
     // console.log('updated array')
   }
+  
+  function playShips() {
+    if (message) 
+      setShipsPlaying((prevIsUpdating) => !prevIsUpdating);
+  }
 
-  // function previousShip() {
-  //   if (!message) {
-  //     console.log('no ships');
-  //     return;
-  //   }
-  //   console.log(counter);
-  //   if (removedShips[-1].index)
-  //   if (shipsShowing.length == counter) {
-  //     console.log('remove ship');
-  //   } else {
-  //     console.log('replace with previous ship')
-  //   }
-  // }
+  useEffect(()=>{
+    let intervalId;
+
+    if (shipsPlaying) {
+      nextShip()
+      intervalId = setInterval(()=>{
+        nextShip();
+      }, 2000)
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [shipsPlaying])
 
   return (
     <Layout className={styles.layout}>
@@ -99,10 +115,6 @@ export default function Simulate() {
 
       {/* TileLayer is the background of the map itself */}
       <Map className={styles.homeMap} width="500" height="200" center={DEFAULT_CENTER} zoom={15}>
-        {/* <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-              /> */}
         {
           ({ TileLayer }) => (
             <>
@@ -111,7 +123,7 @@ export default function Simulate() {
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               />
               {
-                 shipsShowing.map((ship, index) => {
+                shipsArray.map((ship, index) => {
                     return <RoutingMachine key={ship.id} end={BERTH_LOCATIONS[ship.berth-1]} ship={ship}/>
                 })
               }
@@ -123,7 +135,12 @@ export default function Simulate() {
       <div className={styles.bottomContainer}>
         <div className={styles.leftButtons}>
           {/* <button className={styles.simulateButton} onClick={previousShip}> Previous ship</button> */}
-          <button className={styles.simulateButton} onClick={nextShip}> Next ship</button>
+          <button className={styles.simulateButton} onClick={nextShip}>Next ship</button>
+          <button className={styles.simulateButton} onClick={playShips}>
+            {
+              shipsPlaying? <FaPause/> : <FaPlay/>
+            }
+          </button>
           <h3>ETA: {message? message[counter-1].eta: null}</h3>
         </div>
       </div>
