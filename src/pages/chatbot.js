cf403786fb12cc195fbabdcc3fc1a0c7c619562c
xuehuapiaopiaoo <ang_sean7326@hotmail.sg>
2023-10-05 03:46:31 +0800

    updated styling

--- src/pages/chatbot.js
+++ src/pages/chatbot.js
@@ -7,31 +7,10 @@ import Layout from '@components/Layout';
 import { useContext } from 'react';
 import { ShipContext } from 'src/context/shipContext';
 
+import ChatbotLayout from '@components/Chatbot/Chatbot';
 
 function Chatbot() {
-    const [prompt, setPrompt] = useState('');
-    const [botReply, setBotReply] = useState('');
-
-    const { message } = useContext(ShipContext);
-
-    let json;
-
-    if (message) {
-        json = JSON.stringify(message.slice(0, 20))
-    }
-
-    const handleChat = async () => {
-        try {
-            const response = await axios.post('http://127.0.0.1:5000/chat', {
-                json: json,
-                prompt: prompt
-            });
-
-            setBotReply(response.data);
-        } catch (error) {
-            setBotReply("Chat API is not working, please configure your replicate API key")
-        }
-    };
+    
     return (
 
         <Layout>
@@ -41,20 +20,7 @@ function Chatbot() {
                 <meta name="description" content="PSA Codesprint 2023" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className='center rcenter'>
-                <h1 className='cheader'>Welcome to PSA's Chat Bot! Ask any questions regarding the data!</h1>
-                <div>
-                    <input
-                        className='rounded chatinput'
-                        type="text"
-                        value={prompt}
-                        onChange={(e) => setPrompt(e.target.value)}
-                    />
-                    <button type='submit' className='ttext' onClick={handleChat}>Send</button>
-                </div>
-
-                <p className='cbottext'>{botReply ? botReply : "Hi, please ask me any question regarding the dataset!"}</p>
-            </div>
+            <ChatbotLayout />
         </Layout>
 
     )
