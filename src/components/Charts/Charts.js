43c87fb0ed2ca11cfbc8c38507695cbc62157622
chuanshaof <chuanshaof@gmail.com>
2023-10-06 14:13:16 +0800

    updated frontend layout

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -4,27 +4,37 @@ import { Component, useContext } from 'react';
 import { ShipContext } from 'src/context/shipContext';
 
 function RecursiveTable({ data }) {
+  const options = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'GMT',
+    hour12: false, // Disable 12-hour clock
+  };
+  
   return (
     <div>
       <table className=' w-full m-4 gap-2 ttext'>
         <thead className='px-2 justify-center w-16 bg-gray rounded h-300px'>
           <tr>
-            <th className='py-4'>Ship ID</th>
+            <th className='py-4'>Vessel Name</th>
             <th>Berth Number</th>
-            <th>ETA</th>
-            <th>ETD</th>
-            <th>PTD</th>
+            <th>Arrival Time</th>
+            <th>Expected Departure Time</th>
+            <th>Predicted Departure Time</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody className='px-2 justify-center w-16 bg-gray shadow rounded h-300px' >
           {data.map((item) => (
             <tr key={item}>
-              <td className='center font-bold'>{item.id}</td>
+              <td className='center font-bold'>{item.vesselName}</td>
               <td className='center font-bold'>{item.berth}</td>
-              <td className='center font-bold'>{item.eta}</td>
+              <td className='center font-bold'>{new Intl.DateTimeFormat('en-GB', options).format(new Date(item.eta))}</td>
               <td className='center font-bold'>{item.etd}</td>
-              <td className='center font-bold'>{item.ptd}</td>
+              <td className='center font-bold'>{new Intl.DateTimeFormat('en-GB', options).format(new Date(item.ptd))}</td>
               <td className='center font-bold'>{item.predictedDelay}</td>
             </tr>
           ))}
